71222a506e79f50abf6b1ca168a655a219ec77ab
Martin Tracey <matracey@outlook.ie>
2024-09-25 03:44:41 -0700

    feat: add basic react app from template

---
+++ action/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function App() {
+  return (
+    <section className="container mx-auto p-5 fixed inset-0">
+      <div className="w-full h-full flex flex-col">
+        <div className="flex-grow overflow-auto">
+          <h1>Welcome to your new extension!</h1>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default App;
