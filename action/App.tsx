d10d3df2738a0d977d592bdc3858452354ca9061
Martin Tracey <matracey@outlook.ie>
2024-09-26 23:30:13 -0700

    feat: add extension branding to app component

--- action/App.tsx
+++ action/App.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 
 import { Hero } from "react-daisyui";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCode } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
   return (
     <Hero className="py-5 px-2">
       <Hero.Content className="text-center">
         <div className="max-w-md">
+          <FontAwesomeIcon icon={faCode} size="6x" />
           <h1 className="text-2xl font-bold">Welcome to your new extension!</h1>
         </div>
       </Hero.Content>
