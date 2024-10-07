088f06b668c0ba20315391f8b8bf97b858568b4e
Martin Tracey <matracey@outlook.ie>
2024-10-06 19:54:40 -0700

    feat: refactor task to use queue over array

--- scripts/models/common/task.ts
+++ scripts/models/common/task.ts
@@ -1,8 +1,12 @@
+import { Queue } from "./queue";
+
 export class Task {
   id: string;
   filename: string;
   task: string;
   state: string;
+
+  private static readonly TASKS_KEY: string = "tasks";
 
   constructor(filename: string, task: string, state: string) {
     this.id = self.crypto.randomUUID();
@@ -12,42 +16,52 @@ export class Task {
   }
 
   /**
-   * Add task to task list
+   * Add a Task to the front of the task Queue.
    *
-   * @param task
+   * @param task The Task to add to the Queue.
+   * @returns A promise that resolves to the Id of the enqueued Task.
    */
-  public static async addTask(task: Task): Promise<string> {
-    const tasks = await Task.fetchTasks();
-    tasks.unshift(task);
-    await browser.storage.local.set({ tasks: tasks });
+  static async addTask(task: Task): Promise<string> {
+    const tasks = await Task.fetchAll();
+    tasks.enqueue(task);
+    await Task.flush(tasks);
     return task.id;
   }
 
   /**
-   * Update state of task
+   * Update the state of a given task.
    *
-   * @param id
-   * @param state
+   * @param id The Id of the Task to update.
+   * @param state The updated state to set on the Task.
    */
-  public static async updateTask(id: string, state: string) {
-    const tasks = await Task.fetchTasks();
+  static async updateTask(id: string, state: string) {
+    const tasks = await Task.fetchAll();
     for (const task of tasks) {
       if (task.id === id) {
         task.state = state;
       }
     }
-    await browser.storage.local.set({ tasks: tasks });
+    await Task.flush(tasks);
   }
 
   /**
-   * Fetch active tasks
+   * Fetches all active tasks from localStorage.
+   * 
+   * @returns A Queue that contains all active tasks.
    */
-  private static async fetchTasks(): Promise<Array<Task>> {
-    const storageResponse = await browser.storage.local.get("tasks");
-    if (storageResponse.tasks) {
-      return storageResponse.tasks;
-    } else {
-      return [];
-    }
+  private static async fetchAll(): Promise<Queue<Task>> {
+    const storageResponse = await browser.storage.local.get(Task.TASKS_KEY);
+
+    return Queue.fromArray(storageResponse?.tasks ?? []);
+  }
+
+  /**
+   * Flushes the specified taskQueue to localStorage.
+   * 
+   * @param taskQueue The taskQueue to flush.
+   * @returns A promise that resolves when localStorage has been updated.
+   */
+  private static async flush(taskQueue: Queue<Task>): Promise<void> {
+    return await browser.storage.local.set({ tasks: [...taskQueue] });
   }
 }
