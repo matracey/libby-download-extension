841dd23d841fea1744293d7689c46cb315e64279
Martin Tracey <matracey@outlook.ie>
2024-10-08 16:00:26 -0700

    feat: add ContentFormatTypeName enum

---
+++ scripts/models/enums/ContentFormatTypeName.ts
@@ -0,0 +1,8 @@
+export enum ContentFormatTypeName {
+  epubEBook = "EPUB eBook",
+  kindleBook = "Kindle Book",
+  koboEBook = "Kobo eBook",
+  mp3Audiobook = "MP3 audiobook",
+  overDriveListenAudiobook = "OverDrive Listen audiobook",
+  overDriveRead = "OverDrive Read",
+}
