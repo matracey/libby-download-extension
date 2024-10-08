8af51c3c258f1948c9466132e5cb133da8ce162f
Martin Tracey <matracey@outlook.ie>
2024-10-08 16:02:19 -0700

    feat: add ContentFormatTypeId enum

---
+++ scripts/models/enums/ContentFormatTypeId.ts
@@ -0,0 +1,8 @@
+export enum ContentFormatTypeId {
+  audiobookMp3 = "audiobook-mp3",
+  audiobookOverdrive = "audiobook-overdrive",
+  ebookEpubAdobe = "ebook-epub-adobe",
+  ebookKindle = "ebook-kindle",
+  ebookKobo = "ebook-kobo",
+  ebookOverdrive = "ebook-overdrive",
+}
