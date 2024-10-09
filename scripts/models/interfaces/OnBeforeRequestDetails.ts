63964e253d7db1a09c0d846a469026d4329d1727
Martin Tracey <matracey@outlook.ie>
2024-10-08 18:23:25 -0700

    feat: add IGetRequestDetails interface

--- scripts/models/interfaces/OnBeforeRequestDetails.ts
+++ scripts/models/interfaces/OnBeforeRequestDetails.ts
@@ -1,1 +1,7 @@
 export type OnBeforeRequestDetails = browser.webRequest._OnBeforeRequestDetails;
+
+export interface IGetRequestDetails extends OnBeforeRequestDetails {
+  method: "GET";
+}
+
+export const isGetRequestDetails = (d: OnBeforeRequestDetails): d is IGetRequestDetails => d.method == "GET";
