8d36b3b981dbe01d8af5da9ec19ae714397c0473
Martin Tracey <matracey@outlook.ie>
2024-10-08 18:23:26 -0700

    feat: add ISentrySyncRequestDetails interface

--- scripts/models/interfaces/ISentryRequestDetails.ts
+++ scripts/models/interfaces/ISentryRequestDetails.ts
@@ -1,6 +1,11 @@
-import { type OnBeforeRequestDetails } from "./OnBeforeRequestDetails";
+import { type IGetRequestDetails, isGetRequestDetails, type OnBeforeRequestDetails } from "./OnBeforeRequestDetails";
 
 export interface ISentryRequestDetails extends OnBeforeRequestDetails {}
 
 export const isSentryRequestDetails = (d: OnBeforeRequestDetails): d is ISentryRequestDetails =>
   new URL(d.url).host === "sentry.libbyapp.com";
+
+export interface ISentrySyncRequestDetails extends IGetRequestDetails {}
+
+export const isSentrySyncRequestDetails = (d: OnBeforeRequestDetails): d is ISentrySyncRequestDetails =>
+  isGetRequestDetails(d) && isSentryRequestDetails(d) && new URL(d.url).pathname.endsWith("/sync");
