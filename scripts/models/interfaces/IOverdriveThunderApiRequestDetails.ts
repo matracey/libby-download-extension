bbcaa70df905357e3f6b17ba28a7bf9003aca77b
Martin Tracey <matracey@outlook.ie>
2024-10-08 18:43:06 -0700

    feat: add IOverdriveThunderApiLibrariesGetRequestDetails interface

--- scripts/models/interfaces/IOverdriveThunderApiRequestDetails.ts
+++ scripts/models/interfaces/IOverdriveThunderApiRequestDetails.ts
@@ -1,4 +1,4 @@
-import { type OnBeforeRequestDetails } from "./OnBeforeRequestDetails";
+import { type IGetRequestDetails, type OnBeforeRequestDetails, isGetRequestDetails } from "./OnBeforeRequestDetails";
 
 export interface IOverdriveThunderApiRequestDetails extends OnBeforeRequestDetails {}
 
@@ -6,3 +6,10 @@ export const isOverdriveThunderApiRequestDetails = (
   d: OnBeforeRequestDetails
 ): d is IOverdriveThunderApiRequestDetails =>
   new URL(d.url).host === "thunder.api.overdrive.com" && new URL(d.url).pathname.startsWith("v2");
+
+export interface IOverdriveThunderApiLibrariesGetRequestDetails extends IOverdriveThunderApiRequestDetails {}
+
+export const isOverdriveThunderApiLibrariesGetRequestDetails = (
+  d: OnBeforeRequestDetails
+): d is IOverdriveThunderApiLibrariesGetRequestDetails | IGetRequestDetails =>
+  isGetRequestDetails(d) && isOverdriveThunderApiRequestDetails(d) && new URL(d.url).pathname.endsWith("/libraries");
